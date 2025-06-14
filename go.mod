cc91b381cb324ace477bc3808a84af75e2b9ffbb
Doyoung Kim <lark.noanswer@gmail.com>
2025-06-14 12:21:31 +0900

    set up connectrpc

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module coupon-issuance
 
 go 1.22.3
+
+require connectrpc.com/connect v1.16.0
+
+require google.golang.org/protobuf v1.33.0 // indirect
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	coupon "coupon-issuance/gen/coupon/v1"
+
+	"connectrpc.com/connect"
+)
+
+type CouponServer struct{}
+
+func (s *CouponServer) CreateCampaign(ctx context.Context, req *connect.Request[coupon.CreateCampaignRequest]) (*connect.Response[coupon.CreateCampaignResponse], error) {
+	// TODO: Implement campaign creation logic
+	return connect.NewResponse(&coupon.CreateCampaignResponse{}), nil
+}
+
+func (s *CouponServer) GetCampaign(ctx context.Context, req *connect.Request[coupon.GetCampaignRequest]) (*connect.Response[coupon.GetCampaignResponse], error) {
+	// TODO: Implement campaign retrieval logic
+	return connect.NewResponse(&coupon.GetCampaignResponse{}), nil
+}
+
+func (s *CouponServer) IssueCoupon(ctx context.Context, req *connect.Request[coupon.IssueCouponRequest]) (*connect.Response[coupon.IssueCouponResponse], error) {
+	// TODO: Implement coupon issuance logic
+	return connect.NewResponse(&coupon.IssueCouponResponse{}), nil
+}
+
+func main() {
+	server := &CouponServer{}
+	path, handler := coupon.NewCouponServiceHandler(server)
+	mux := http.NewServeMux()
+	mux.Handle(path, handler)
+
+	log.Printf("Server listening at http://localhost:8080")
+	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
